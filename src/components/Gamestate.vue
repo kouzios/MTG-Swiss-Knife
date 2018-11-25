f56b3236b1292a46927eab67978e414df850593c
Matthew Kouzios <mkouzios@hotmail.com>
2018-11-24 22:53:14 -0600

    Color changing functionality fixed
    Previously changed input fields, but that causes a loss of focus, so now there is an observer that reacts to the change

--- src/components/Gamestate.vue
+++ src/components/Gamestate.vue
@@ -2,9 +2,19 @@
     <div>
         <div class='row'>
             <div class='player card col-6' v-for="(player, index) in players" :key="'player' + index">
-                <input class='name-input' type='text' :value="player.name" autocomplete="off"/>
+                <input class='name-input' type='text' v-model="player.name" autocomplete="off"/>
                 <hr>
-                <input class='life-input' type='text' :value="player.life" autocomplete="off"/>
+                <div class='row point-area'>
+                    <div class='col-4 btn point-alteration' @click='decriment(index)'>
+                        -
+                    </div>
+                    <div class='col-4'>
+                        <input class='life-input mw-100' type='text' v-model="player.life" :style='{color: player.lifeColor}' autocomplete="off"/>
+                    </div>
+                    <div class='col-4 btn point-alteration' @click='incriment(index)'>
+                        +
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,21 +27,51 @@ export default {
             players: [
                 {
                     name: "Player 1",
-                    life: 40
+                    life: 40,
+                    lifeColor: "black"
                 },
                 {
                     name: "Player 2",
-                    life: 40
+                    life: 40,
+                    lifeColor: "black"
                 },
                 {
                     name: "Player 3",
-                    life: 40
+                    life: 40,
+                    lifeColor: "black"
                 },
                 {
                     name: "Player 4",
-                    life: 40
+                    life: 40,
+                    lifeColor: "black"
                 }
             ]
+        }
+    },
+    methods: {
+        decriment(index) {
+            this.players[index].life--
+        },
+        incriment(index) {
+            this.players[index].life++
+        }
+    },
+    watch: {
+        players: {
+            handler: function(val) {
+                for(var i = 0; i < this.players.length; i++) {
+                    var life = val[i].life;
+                    if(life > 40) {
+                        this.players[i].lifeColor = "green";
+                    } else if (life == 40) {
+                        this.players[i].lifeColor = "black";
+
+                    } else {
+                        this.players[i].lifeColor = "red";
+                    }
+                }
+            },
+            deep: true
         }
     }
 }
@@ -41,18 +81,37 @@ export default {
 .player {
     box-shadow: 2px 2px lightgray;
     text-align: center;
+    padding-right: 0px !important;
 }
 
 .life-input {
     text-align: center;
     font-size: 32px;
-    color: green;
     border: none !important;
+    background-color: #f8f8f8;
+    box-sizing: border-box;
+}
+
+.surplus-life {
+ color: green;
+}   
+
+.deficit-life {
+    color: red;
 }
 
 .name-input {
     text-align: center;
     font-size: 32px;
     border: none !important;
+}
+
+.point-alteration {
+    font-size: 24px;
+    background-color: #f1f1f1 !important;
+}
+
+.point-area {
+    background-color: #f8f8f8;
 }
 </style>
