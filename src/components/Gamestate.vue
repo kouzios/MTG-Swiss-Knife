c8e7eb15a7c79619d78a4f3ad4a3ea9125da68d7
Matthew Kouzios <mkouzios@hotmail.com>
2018-12-10 22:18:56 -0600

    Added tooltips to player cards

--- src/components/Gamestate.vue
+++ src/components/Gamestate.vue
@@ -3,7 +3,7 @@
         <div class='row'>
             <div class='player card col-6'  :class="{flipped: player.isFlipped}" v-for="(player, index) in players" :key="'player' + index">
                 <div class='d-flex flex-row'>
-                    <font-awesome-icon class='clickable flip' icon="level-up-alt" @click="player.isFlipped = !player.isFlipped"/>
+                    <font-awesome-icon class='clickable flip' icon="level-up-alt" v-b-tooltip.hover.bottom="'Flip View'" @click="player.isFlipped = !player.isFlipped"/>
                     <input class="name-input w-100" type='text' v-model="player.name" autocomplete="off"/>
                 </div>
                 <Lifestate :defaultLife='defaultLife' :player='player' :index='index'/>
